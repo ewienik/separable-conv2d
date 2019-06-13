c7f32ab95f4ba949b67640a4899a673241ce8e57
Pawel Pery <ewienik@post.pl>
2019-06-14 00:05:04 +0200

    Introduce comparing outputs

--- testsepconv.cpp
+++ testsepconv.cpp
@@ -7,6 +7,24 @@
 
 /**/
 
+using std::ifstream;
+using std::string;
+using std::vector;
+
+/**/
+
+namespace {
+
+    /**/
+
+    constexpr char const * filename = "separable-conv2d-tests.txt";
+
+    /**/
+
+}
+
+/**/
+
 TEST_CASE("Compile simple case") { separableConv2d({1, 1, 1}, {1, 1, 1}, {1, 1}); }
 
 /**/
@@ -20,6 +38,28 @@ TEST_CASE("Wrong number of channels in args") {
 
 TEST_CASE("Correct number of features") {
     REQUIRE(separableConv2d({1, 1, 1}, {1, 1, 1}, {3, 1}).size(0) == 3);
+}
+
+/**/
+
+TEST_CASE("Compare outputs") {
+    static auto files = vector<string>{};
+    static auto initialized = false;
+    if (!initialized) {
+        auto istrm = ifstream(filename);
+        REQUIRE(istrm.is_open());
+        while (!istrm.eof()) {
+            auto file = string{};
+            istrm >> file;
+            files.push_back(file);
+        }
+        initialized = true;
+    }
+    SECTION("Compare with files") {
+        auto id = GENERATE(range(0, static_cast<int>(files.size())));
+        auto path = files[id];
+        REQUIRE(!path.empty());
+    }
 }
 
 /**/
